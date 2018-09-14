7c59e929a8f8cbb47d0e919c975b6f2672c4395f
stevenliu-sl <stevenliu.sl01@gmail.com>
2018-09-13 22:39:12 -0400

    First version of the first homework assignment

---
+++ FirstProject.java
@@ -0,0 +1,21 @@
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		String name = "Tom";
+		System.out.println("My friend's name is " + name + ".");
+		int width = 3;
+		int length = 4;
+		int area = length*width;
+		System.out.println("The area of a rectangle with a length of " + length
+							+ " and a width of " + width + " is equal to " + area + ".");
+		int radius = 5;
+		double pi = 3.14;
+		double circumference = 2*radius*pi;
+		System.out.println("The cirumference of a circle with a radius of " + radius
+				+ " is about " + circumference + ".");
+		
+
+	}
+
+}
